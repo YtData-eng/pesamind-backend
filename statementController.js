2d36427b27b938c8e1a3204fcf37060c32ea756b
Tracy <tracyyegon857@gmail.com>
2026-05-30 11:12:39 +0300

    Fix auth middleware

--- statementController.js
+++ statementController.js
@@ -15,7 +15,7 @@ const authenticate = (req, res, next) => {
   const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = { id: decoded.userId || decoded.id };
     next();
   } catch (err) {
     return res.status(401).json({ error: 'Invalid token' });
